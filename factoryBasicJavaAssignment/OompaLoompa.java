fd7050225781971726c6f5802e92226e8a4271ef
mbohekale <mbohekale@yahoo.com>
2018-12-07 18:45:54 +0100

    Delete OompaLoompa.java

--- factoryBasicJavaAssignment/OompaLoompa.java
+++
@@ -1,8 +0,0 @@
-public class OompaLoompa extends Person{
-	public OompaLoompa(String code, String name,int height, String favoritefood ){
-		super(name);
-		this.code=code;
-		this.height=height;
-		this.favoritefood=favoritefood;
-	}
-}
